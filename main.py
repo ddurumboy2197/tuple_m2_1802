2a620cb5282d605f1f6c591adacbdc5106528a44
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-02-18 15:23:55 +0500

    Manage colors list with addition, removal, and sorting

---
+++ main.py
@@ -0,0 +1,11 @@
+colors = ('red', 'blue', 'green')
+
+colors_list = list(colors)
+
+colors_list.append('yellow')
+
+colors_list.remove('blue')
+
+colors_list.sort()
+
+print("\n2-MASALA yakuniy ro‘yxat:", colors_list)
